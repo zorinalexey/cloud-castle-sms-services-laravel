67e30e8afe35569334a038b6571e87b5a6896f07
Алексей <74205167+zorinalexey@users.noreply.github.com>
2023-04-06 10:11:01 +0400

    Update Sms.php

--- src/Models/Sms.php
+++ src/Models/Sms.php
@@ -2,10 +2,8 @@
 
 namespace CloudCastle\SmsServices\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 final class Sms extends UuidModel
 {
-    use HasFactory;
+    
 }
